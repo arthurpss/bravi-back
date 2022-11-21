fb5708b7fd63da05f0300f7e2afb8d7963a51a73
Arthur Passos <arthur.pcorrea@sempreceub.com>
2022-11-21 17:18:17 -0300

    :rocket: set server port

--- src/server.ts
+++ src/server.ts
@@ -16,8 +16,8 @@ AppDataSource.initialize()
     app.use(express.json());
     app.use(routes);
 
-    app.listen(8080, () => {
-      console.log("Server started on port 8080!");
+    app.listen(process.env.PORT || 8080, () => {
+      console.log("Server started on port", process.env.PORT || 8080);
     });
   })
   .catch((err) => {
